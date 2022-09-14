e9d29f48a2a51e55477c7ae9ef5a8b81fff70c43
gonza <gonzalorom2001@gmail.com>
2022-09-14 17:02:24 -0300

    itemdetail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ItemListContainer/>
+        <ItemDetailContainer/>
       </header>
     </div>
   );
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,27 @@
+import data from '../mockData';
+import { useEffect, useState } from 'react';
+import ItemList from '../ItemList/itemList';
+
+const ItemDetailContainer = ({greeting}) => {
+    const [productList, setProductList] = useState ([]);
+
+    useEffect(() => {
+        getProducts.then((response) => {
+            setProductList(response);
+        })
+    },[])
+    
+    const getProducts = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(data.filter ((prod) => prod.id === '3'))
+            }, 2000);
+        });
+
+    return (
+        <>
+            <ItemList lista={productList}/>
+        </>
+    )
+}
+
+export default ItemDetailContainer
